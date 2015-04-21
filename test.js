df3a5d626286eae15b989268b8a3efe891c03309
Guilherme Vieira <super.driver.512@gmail.com>
2015-04-21 02:06:57 -0300

    Implement typewriter.lineBreak.

--- test.js
+++ test.js
@@ -6,13 +6,13 @@ clear();
 tw.type({d:100}, "August 3, 9:47 AM", {w:500});
 tw.type("District Court");
 tw.type("Defendant Lobby No. 2", {w:2000});
-tw.type();
+tw.lineBreak();
 tw.type({who:"Phoenix"}, {dh:true}, {d:30}, "(Boy am I nervous!)", {w:1500});
-tw.type();
+tw.lineBreak();
 tw.type({who:"Mia"}, {dh:true}, {d:40}, "Wright!", {w:1250});
 tw.type({di:true}, {d:50}, "Did you", {w:1000}, {d:30}, " turn off the lights?", {w:1000});
 tw.type({di:true}, {d:400}, ". . .", {w:1000});
 tw.type({di:true}, {d:40}, "I can't see a thing!", {w:1000});
-tw.type().then(function() {
+tw.lineBreak().then(function() {
 	process.exit();
 });
