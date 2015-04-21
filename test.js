940fa03302a857e1e7224ffc7068efa50a574954
Guilherme Vieira <super.driver.512@gmail.com>
2015-04-21 00:52:15 -0300

    Rename typewriter.typeSpeech to typeDialogue.

--- test.js
+++ test.js
@@ -8,11 +8,11 @@ tw.type("August 3, 9:47 AM", {w:500}, "\n");
 tw.type("District Court\n");
 tw.type("Defendant Lobby No. 2", {w:2000}, "\n");
 tw.type({d:0}, "\n");
-tw.typeSpeech("Phoenix", [
+tw.typeDialogue("Phoenix", [
 	[{d:30}, "(Boy am I nervous!)", {w:1500}]
 ]);
 tw.type({d:0}, "\n");
-tw.typeSpeech("Mia", [
+tw.typeDialogue("Mia", [
 	[{d:40}, "Wright!", {w:1250}],
 	[{d:50}, "Did you", {w:1000}, {d:30}, " turn off the lights?", {w:1000}],
 	[{d:400}, ". . .", {w:1000}],
