e1ca960962b98222d0a0f0bcdcb59769719b44a4
Guilherme Vieira <super.driver.512@gmail.com>
2015-04-21 02:25:19 -0300

    Test timing improvement.

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ tw.lineBreak();
 tw.type({who:"Phoenix"}, {dh:true}, {d:30}, "(Boy am I nervous!)", {w:1500});
 tw.lineBreak();
 tw.type({who:"Mia"}, {dh:true}, {d:40}, "Wright!", {w:1250});
-tw.type({di:true}, {d:50}, "Did you", {w:1000}, {d:30}, " turn off the lights?", {w:1000});
+tw.type({di:true}, {d:50}, "Did you", {w:1000}, {d:0}, " ", {d:30}, "turn off the lights?", {w:1000});
 tw.type({di:true}, {d:400}, ". . .", {w:1000});
 tw.type({di:true}, {d:40}, "I can't see a thing!", {w:1000});
 tw.lineBreak().then(function() {
