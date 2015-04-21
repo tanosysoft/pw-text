d315e0f5079faa5a92548d65ae5484e056997abc
Guilherme Vieira <super.driver.512@gmail.com>
2015-04-21 01:36:39 -0300

    refactor: Automatic line breaks and simpler dialogue API.

--- test.js
+++ test.js
@@ -3,21 +3,17 @@
 var clear = require('clear');
 var tw = require('./lib/typewriter');
 clear();
-tw.type({d:100});
-tw.type("August 3, 9:47 AM", {w:500}, "\n");
-tw.type("District Court\n");
-tw.type("Defendant Lobby No. 2", {w:2000}, "\n");
-tw.type({d:0}, "\n");
-tw.typeDialogue("Phoenix", [
-	[{d:30}, "(Boy am I nervous!)", {w:1500}]
-]);
-tw.type({d:0}, "\n");
-tw.typeDialogue("Mia", [
-	[{d:40}, "Wright!", {w:1250}],
-	[{d:50}, "Did you", {w:1000}, {d:30}, " turn off the lights?", {w:1000}],
-	[{d:400}, ". . .", {w:1000}],
-	[{d:40}, "I can't see a thing!", {w:1000}],
-]);
-tw.type({d:0}, "\n").then(function() {
+tw.type({d:100}, {hl:true});
+tw.type("August 3, 9:47 AM", {w:500});
+tw.type("District Court");
+tw.type("Defendant Lobby No. 2", {w:2000});
+tw.type();
+tw.type({who:"Phoenix"}, {dh:true}, {d:30}, "(Boy am I nervous!)", {w:1500});
+tw.type();
+tw.type({who:"Mia"}, {dh:true}, {d:40}, "Wright!", {w:1250});
+tw.type({di:true}, {d:50}, "Did you", {w:1000}, {d:30}, " turn off the lights?", {w:1000});
+tw.type({di:true}, {d:400}, ". . .", {w:1000});
+tw.type({di:true}, {d:40}, "I can't see a thing!", {w:1000});
+tw.type().then(function() {
 	process.exit();
 });
