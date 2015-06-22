ed55d946fc0804929d1faf968c8c3eb4f3e2724d
Guilherme Vieira <super.driver.512@gmail.com>
2015-06-22 02:40:35 -0300

    Remove old-version directory.

--- old-version/lib/logQUnhandledReasonsOnExit.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var q = require('q');
-process.on('exit', function() {
-	q.getUnhandledReasons().forEach(function(reason) {
-		console.error("The follwing promise rejection reason went unhandled:");
-		console.error(reason);
-	});
-});
--- old-version/test-script-runner.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env iojs
-'use strict';
-var fs = require('fs');
-var run = require('./lib/novel-cli-script-runner').run;
-run(fs.readFileSync('test-script.nclis', { encoding: 'utf8' }));
