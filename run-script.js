1f7f23e46193e17d5bd73a46a064c05a8dbac764
Guilherme Vieira <super.driver.512@gmail.com>
2015-06-22 02:34:49 -0300

    Completely rewritten using CHAINScript.

---
+++ run-script.js
@@ -0,0 +1,18 @@
+'use strict';
+let fs = require('fs');
+let commandQueue = require('chain/command-queue');
+commandQueue.registerCommandHandlers(require('chain/io'));
+let compile = require('chain/compile-script');
+exports = module.exports = function(path, startingLabel) {
+	let script = compile (
+		fs.readFileSync(path, { encoding: 'utf8' })
+	);
+	script.runFile = function(path) {
+		exports(path).done();
+		this.exit();
+	};
+	return script.run(startingLabel);
+};
+if(!module.parent) {
+	exports(process.argv[2], process.argv[3]).done();
+}
