b9c2bcf57cd7aa12777b16676f366126a5936c5e
Guilherme Vieira <super.driver.512@gmail.com>
2015-06-27 19:29:19 -0300

    run-script: Implement return labels, return function, and script stacking.

--- run-script.js
+++ run-script.js
@@ -6,7 +6,10 @@ let io = require('chain/io');
 commandQueue.registerCommandHandlers(io);
 commandQueue.push({'no-skip':true});
 let compile = require('chain/compile-script');
-exports = module.exports = function(path, startingLabel) {
+let latestScript;
+let scriptStack = [];
+exports = module.exports = function(path, more) {
+	more = more || {};
 	let script = compile (
 		fs.readFileSync(path, { encoding: 'utf8' })
 	);
@@ -14,8 +17,29 @@ exports = module.exports = function(path, startingLabel) {
 		exports.apply(this, arguments).done();
 		this.exit();
 	};
-	return script.run(startingLabel);
+	script.return = function() {
+		let previousScript = scriptStack.pop();
+		if(!previousScript) {
+			throw new Error("Can't return: The script stack is empty");
+		}
+		this.exit();
+		latestScript = previousScript.script;
+		previousScript.script.run(previousScript.returnLabel);
+	};
+	if(more.returnLabel) {
+		if(!latestScript) {
+			throw new Error("Return label specified, but no script is running");
+		}
+		scriptStack.push ({
+			script: latestScript,
+			returnLabel: more.returnLabel,
+		});
+	}
+	latestScript = script;
+	return script.run(more.startingLabel);
 };
 if(!module.parent) {
-	exports(process.argv[2], process.argv[3]).done();
+	exports(process.argv[2], {
+		startingLabel: process.argv[3]
+	}).done();
 }
